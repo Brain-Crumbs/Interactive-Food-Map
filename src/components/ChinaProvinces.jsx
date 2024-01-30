fb3db882bdb5d87954e55f713724918f0bef684b
Brain-Crumbs <55927359+Brain-Crumbs@users.noreply.github.com>
2024-01-30 05:02:03 +0000

    Pending changes exported from your codespace

--- src/components/ChinaProvinces.jsx
+++ src/components/ChinaProvinces.jsx
@@ -1,69 +1,63 @@
-import React, { useRef, useEffect } from 'react';
-import { GeoJSON, TileLayer, useMap  } from 'react-leaflet';
-import L from 'leaflet'
-import { useLeafletContext } from '@react-leaflet/core'
+import React, { useRef, useState, useEffect } from 'react';
+import { GeoJSON, useMap } from 'react-leaflet';
+import L from 'leaflet';
 import 'leaflet/dist/leaflet.css';
 import chinaProvinces from '../data/chinaProvincesL2GeoData.json';
-
-function highlightFeature(e) {
-  var layer = e.target;
-
-  layer.setStyle({
-    weight: 1,
-    color: 'black',
-    dashArray: '',
-    fillOpacity: 0.7
-  });
-
-  if (!L.Browser.ie && !L.Browser.opera && !L.Browser.edge) {
-    layer.bringToFront();
-  }
-}
 
 const ChinaProvinces = () => {
   const geoJsonRef = useRef();
   const map = useMap();
   if (map) {
-
     const osm = new L.TileLayer.boundaryCanvas(
       "https://{s}.basemaps.cartocdn.com/light_nolabels/{z}/{x}/{y}{r}.png",
       {
         boundary: chinaProvinces,
-        attribution:
-          '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors, UK shape <a href="https://github.com/johan/world.geo.json">johan/word.geo.json</a>'
+        attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
       }
     );
     map.addLayer(osm);
-    const ukLayer = L.geoJSON(chinaProvinces);
-    map.fitBounds(ukLayer.getBounds());
+    const provincesLayer = L.geoJSON(chinaProvinces);
+    map.fitBounds(provincesLayer.getBounds());
+  }
 
-    // // Define the coordinates for the large rectangle covering the entire globe
-    // const outerBounds = [[90, -180], [90, 180], [-90, 180], [-90, -180]];
 
-    // // Assuming chinaProvinces is your GeoJSON data
-    
-    const chinaProvincesLatLong = L.geoJson(chinaProvinces).getLayers().map(layer => layer.getLatLngs());
+  function highlightFeature(e) {
+    var layer = e.target;
 
-    // // Create the inverted polygon using the outer bounds and the GeoJSON-defined areas as holes
-    // const invertedPolygon = L.polygon([outerBounds, ...chinaProvincesLatLong], {
-    //   color: "#000", // Match the map's background color
-    //   fillColor: "#000",
-    //   fillOpacity: 0.7,
-    //   weight: 0
-    // }).addTo(map);
-    map.setMaxBounds(chinaProvincesLatLong);
-    map.setMinZoom(4);
-    map.setMaxZoom(8);
+    layer.setStyle({
+      fillColor: 'darkRed', 
+      weight: 2,
+      opacity: 1,
+      color: 'white',
+      fillOpacity: 1
+    });
+
+    if (!L.Browser.ie && !L.Browser.opera && !L.Browser.edge) {
+      layer.bringToFront();
+    }
   }
 
   function resetHighlight(e) {
-    const layer = e.target;
-    geoJsonRef.current.resetStyle(layer);
+    geoJsonRef.current.resetStyle(e.target);
   }
 
   function zoomToFeature(e) {
     const layer = e.target;
     map.fitBounds(layer.getBounds());
+    
+    geoJsonRef.current.setStyle({fillColor: 'darkRed', 
+    weight: 2,
+    opacity: 1,
+    color: 'white',
+    fillOpacity: 1});
+
+    layer.setSyle( {
+      fillColor: 'white', 
+      weight: 10,
+      opacity: 1,
+      color: 'black',
+      fillOpacity: 0
+    });
   }
 
   function onEachFeature(feature, layer) {
@@ -76,19 +70,16 @@ const ChinaProvinces = () => {
 
   function mapStyle(feature) {
     return {
-      fillColor: 'darkred',
+      fillColor: 'darkRed', 
       weight: 2,
       opacity: 1,
-      color: 'black',
-      fillOpacity: 0.2
+      color: 'white',
+      fillOpacity: 0.4
     };
   }
 
   return (
-    <div>
-      
-      {chinaProvinces && <GeoJSON ref={geoJsonRef} data={chinaProvinces} style={mapStyle} onEachFeature={onEachFeature} />}
-    </div>
+    <GeoJSON ref={geoJsonRef} data={chinaProvinces} style={mapStyle} onEachFeature={onEachFeature} />
   );
 };
 
